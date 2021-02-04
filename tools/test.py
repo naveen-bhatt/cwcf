cfc90925304a0c0bd5dcba807041783ba1341e9b
Naveen <naveenbhatt.d@gmail.com>
2021-02-04 14:23:09 +0530

    added confusion matrix code in hpc_svm.py

---
+++ tools/test.py
@@ -0,0 +1,5 @@
+rom sklearn.model_selection import cross_val_predict
+y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
+
+from sklearn.metrics import confusion_matrix
+confusion_matrix(y_train_5, y_train_pred)
